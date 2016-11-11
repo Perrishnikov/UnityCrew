8f36768ffe5e901c08d9788179e7bd94db71056f
Perry <perry@Perrys-MacBook-Pro.local>
2016-11-11 13:38:32 -0600

    dummyScript

---
+++ Unity Club 02/Assets/DummyScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		Debug.Log("Hello World!");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
