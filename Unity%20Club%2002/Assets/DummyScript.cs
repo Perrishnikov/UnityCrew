ed0276c3dcebda1ef0b64e0813db688f0c20b767
ChrisMenning <chrismenning@gmail.com>
2016-11-11 14:53:04 -0600

    Add files via upload

---
+++ Unity%20Club%2002/Assets/DummyScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		Debug.Log("Hello World!");
+		Debug.Log("This is another log");
+        Debug.Log("Chris was here.");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
